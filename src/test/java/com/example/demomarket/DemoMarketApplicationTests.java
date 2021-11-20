a9025c40979dce89aeb089288e17a76d308e9a8a
JFelixZuniga <johnfzuniga@gmail.com>
2021-11-19 21:27:47 -0300

    ADD configuración inicial del proyecto

---
+++ src/test/java/com/example/demomarket/DemoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demomarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
